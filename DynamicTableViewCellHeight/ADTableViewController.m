d476b386e60fe3936e12c858903e0b4b46182002
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-03-25 10:02:45 -0400

    Refactor: hard-coded data into a property list
    Moved the data, which was hard-coded in the table view controller, into a
    property list to adhere to MVC pattern.

--- DynamicTableViewCellHeight/ADTableViewController.m
+++ DynamicTableViewCellHeight/ADTableViewController.m
@@ -20,18 +20,10 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  self.dataSource = @[@[@{@"title":@"This is a stock table view cell",
-                          @"detail":@"This proves you can mix custom table view cells with stock ones."}],
-                      @[@{@"title":@"Moby Dick",
-                          @"detail":@"Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world."},
-                        @{@"title":@"A Tale of Two Cities",
-                          @"detail":@"It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way--in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."},
-                        @{@"title":@"The Adventures of Huckleberry Finn",
-                          @"detail":@"YOU don't know about me without you have read a book by the name of \"The Adventures of Tom Sawyer\"; but that ain't no matter."},
-                        @{@"title":@"On the Road",
-                          @"detail":@"I first met Dean not long after my wife and I split up. I had just gotten over a serious illness that I won't bother to talk about, except that it had something to do with the miserably weary split-up and my feeling that everything was dead."},
-                        @{@"title":@"The Catcher in the Rye",
-                          @"detail":@"If you really want to hear about it, the first thing you'll probably want to know is where I was born, and what my lousy childhood was like, and how my parents were occupied and all before they had me, and all that \"David Copperfield\" kind of crap"}]];
+  // Load the data from the resource file Data.plist
+  NSString* resourcePath = [[NSBundle mainBundle] pathForResource:@"Data"
+                                                           ofType:@"plist"];
+  self.dataSource = [NSArray arrayWithContentsOfFile:resourcePath];
 
   // We must set these properties in this method in order for automatic cell
   // sizing to work.
@@ -52,6 +44,8 @@
   UITableViewCell *cell;
   NSDictionary *text = self.dataSource[(NSUInteger)indexPath.section][(NSUInteger)indexPath.row];
 
+  // Here we determine that the first section will use the table view cell
+  // "stock" while the second section will use the table view cell "custom".
   switch (indexPath.section) {
     case 0: {
 
